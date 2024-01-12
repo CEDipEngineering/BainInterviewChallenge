da2a54c92f0cada9f947df77724a308ad2e38454
CEDipEngineering <cadudip@hotmail.com>
2024-01-12 17:19:23 -0300

    Add data abstraction to model folder

---
+++ model/data.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+# Placeholder paths, using pathlib for OS compatibility and robustness
+_train_data_path = Path("../data/train.py").resolve()
+_test_data_path = Path("../data/test.py").resolve()
+
+def load_data() -> tuple[np.ndarray, np.ndarray]:
+    """
+    Loads the train and test data into pandas DataFrames.
+    Can be updated in the future, to access diferent datasources
+    (if data is public, Pandas can actually just receive a URL to 
+     S3 Bucket or Azure Storage Container, which is neat)
+    """
+    train = pd.read_csv(_train_data_path)
+    test = pd.read_csv(_test_data_path)
+    return train, test
